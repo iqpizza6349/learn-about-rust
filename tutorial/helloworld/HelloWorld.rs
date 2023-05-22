ec8b89bfb24d6980fbf752931f95e0572e4a8380
iqpizza6349 <iqpizza6349@gmail.com>
2023-05-22 14:41:37 +0900

    :octocat: :: print 'Hello, Rust!'

---
+++ tutorial/helloworld/HelloWorld.rs
@@ -0,0 +1,10 @@
+
+// function equals to 'fn'
+fn main() {
+    // similar as kotlin
+    println!("Hello, Rust!");
+    println!("why have to wirte '!' in function?");
+    println!("or 'println' is not function at all?");
+    // compile successful with out semicolon
+    println!("what if I do not write ';'in each line?")
+}
