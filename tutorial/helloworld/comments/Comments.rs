d46c2cbee37f73b28cacc394c1d59dd6e803e4fd
iqpizza6349 <iqpizza6349@gmail.com>
2023-05-22 14:42:47 +0900

    :octocat: :: learn comments in rust, also learn how to format

---
+++ tutorial/helloworld/comments/Comments.rs
@@ -0,0 +1,15 @@
+
+fn main() {
+    // comments all same as other some major languages such as java, C++
+    /**
+     * this is block comments!
+     */
+    /*
+    Note: The previous column of `*` was entriely for style. There's
+    no actual need for it.
+     */
+
+    let x = 5 + /* 90 + */ 5;   // warning: use `/* */` for a plain comment. but the tutorial says to do.
+    // It strange to me this format.. but I saw in python.
+    println!("Is `x` is 10 or 100? x = {}", x);
+}
